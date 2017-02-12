b11e7154f78c372432b8174544d14739e4590770
dbdu <1797890817@qq.com>
2017-02-12 16:13:10 +0800

    add lombok and format

--- HRSystem_Eclipse/src/org/crazyit/hrsystem/dao/impl/AttendTypeDaoHibernate4.java
+++ HRSystem_Eclipse/src/org/crazyit/hrsystem/dao/impl/AttendTypeDaoHibernate4.java
@@ -5,16 +5,15 @@ import org.crazyit.hrsystem.dao.AttendTypeDao;
 import org.crazyit.hrsystem.domain.AttendType;
 
 /**
- * Description:
- * <br/>利嫋: <a href="http://www.crazyit.org">決髄Java選男</a>
- * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
- * <br/>This program is protected by copyright laws.
- * <br/>Program Name:
- * <br/>Date:
- * @author  Yeeku.H.Lee kongyeeku@163.com
- * @version  1.0
+ * Description: <br/>
+ * 利嫋: <a href="http://www.crazyit.org">決髄Java選男</a> <br/>
+ * Copyright (C), 2001-2016, Yeeku.H.Lee <br/>
+ * This program is protected by copyright laws. <br/>
+ * Program Name: <br/>
+ * Date:
+ * 
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
  */
-public class AttendTypeDaoHibernate4 extends BaseDaoHibernate4<AttendType>
-	implements AttendTypeDao
-{
+public class AttendTypeDaoHibernate4 extends BaseDaoHibernate4<AttendType> implements AttendTypeDao {
 }
